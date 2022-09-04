47a8b8669a00f40f5a5874625b5e283413dc3b3c
Soumwadeep Guha <soumwadeep@gmail.com>
2022-09-04 13:30:47 +0530

    Added About,Contact

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 // Random Js Here
+
+function googleTranslateElementInit() 
+{
+  new google.translate.TranslateElement({pageLanguage: 'en',
+  includedLanguages:
+  "en,hi,mr,te,ta,as,bn,brx,doi,gu,kn,ks,gom,mai,ml,mni,ne,or,pa,sa,sat,sd,ur"
+   }, 'google_translate_element');
+}
